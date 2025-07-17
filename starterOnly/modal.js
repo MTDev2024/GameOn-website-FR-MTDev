4aefeddc52982b4956363ccc3b61404d1661c5fd
MTDev2024 <mt.dev.2023@gmail.com>
2025-07-17 03:27:35 +0200

    First commit

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -15,6 +15,7 @@ const formData = document.querySelectorAll(".formData");
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
